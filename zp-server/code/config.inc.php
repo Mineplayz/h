d8923dbd030e8ef5c21c3bf6b5cc79991510f671
Mineplayz <45463108+Mineplayz@users.noreply.github.com>
2020-01-28 22:44:37 +0100

    Add files via upload

---
+++ zp-server/code/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ZPS_API_KEY', '919f853c697740388a49d1f49682efd2'); // Hier wird von Zeta Producer der API-Key reingeschrieben.
+define('ZPS_VERSION', '15.0.0.0');
+define('ZPS_DEBUG', false);
+define('ZPS_LOG', true);
+
+?>
